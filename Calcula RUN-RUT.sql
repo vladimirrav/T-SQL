8f2e977fc7b9261c811388d52641bea0d80c471b
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-03-04 19:25:51 -0300

    Cálculo de RUT/RUN - Chile
    RUT - Rol Único Tributário
    RUN - Rol Único Nacional

---
+++ Calcula RUN-RUT.sql
@@ -0,0 +1,30 @@
+print 'Chile - Cálculo de RUT/RUN';
+
+declare @Rut as int = 12345678;
+
+declare @Digito as integer,
+		@Contador as integer = 2,
+		@Multiplo as integer,
+		@Acumulador as integer = 0,
+		@retorno as varchar(1),
+		@CalcRUT as int = @Rut;
+
+while @CalcRUT <> 0
+begin
+	set @Multiplo = (@CalcRUT % 10) * @Contador;
+	set @Acumulador = @Acumulador + @Multiplo;
+	set @CalcRUT = @CalcRUT / 10;
+	set @Contador = @Contador + 1;
+	if @Contador > 7
+		set @Contador = 2;
+end;
+
+set @Digito = 11 - (@Acumulador % 11);
+
+select @retorno = case
+					when @Digito = 10 then 'K' 
+					when @Digito = 11 then '0'
+					else cast(@Digito as varchar(1))
+				end;
+
+print concat('RUT: ', format(@Rut, '00,000,000', 'es-cl'), '-', @retorno);
