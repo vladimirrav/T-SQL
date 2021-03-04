d1f1348d2a27b49bd10179c8867c38ee651d30de
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-03-04 14:57:08 -0300

    Dynamic list of table columns
    Improvements in the main query.

--- Dynamic list of table columns.sql
+++ Dynamic list of table columns.sql
@@ -2,41 +2,40 @@ use DBName;
 
 print 'Dynamic list of table columns';
 
-select
-	TABLE_CATALOG,
-	TABLE_SCHEMA,
-	TABLE_NAME,
-	concat('select ',
-		string_agg(
-			--Check for invalid name
-			iif(charindex(' ', COLUMN_NAME) > 0 or charindex('/', COLUMN_NAME) > 0 or COLUMN_NAME like '[0-9]%',
-				quotename(COLUMN_NAME),
-				COLUMN_NAME
-			),
-			', '
-		) within group (order by ORDINAL_POSITION),
-		char(13), char(10),
-		'from ',
+;with list as
+(
+	select
+		TABLE_CATALOG,
 		iif(charindex(' ', TABLE_SCHEMA) > 0 or charindex('/', TABLE_SCHEMA) > 0 or TABLE_SCHEMA like '[0-9]%',
-				quotename(TABLE_SCHEMA),
-				TABLE_SCHEMA
-			),
-		'.', 
+					quotename(TABLE_SCHEMA),
+					TABLE_SCHEMA
+				) as TABLE_SCHEMA,
 		iif(charindex(' ', TABLE_NAME) > 0 or charindex('/', TABLE_NAME) > 0 or TABLE_NAME like '[0-9]%',
-				quotename(TABLE_NAME),
-				TABLE_NAME
-			),
-		';'
-	) as cmd_select,
-	string_agg(
-			iif(charindex(' ', COLUMN_NAME) > 0 or charindex('/', COLUMN_NAME) > 0 or COLUMN_NAME like '[0-9]%',
-				quotename(COLUMN_NAME),
-				COLUMN_NAME
-			),
-			', '
-		) within group (order by ORDINAL_POSITION) as columns_list
-from INFORMATION_SCHEMA.COLUMNS
-group by
-	TABLE_CATALOG,
-	TABLE_SCHEMA,
-	TABLE_NAME;
+					quotename(TABLE_NAME),
+					TABLE_NAME
+				) as TABLE_NAME,
+		string_agg(
+				iif(charindex(' ', COLUMN_NAME) > 0 or charindex('/', COLUMN_NAME) > 0 or COLUMN_NAME like '[0-9]%',
+					quotename(COLUMN_NAME),
+					COLUMN_NAME
+				),
+				', '
+			) within group (order by ORDINAL_POSITION) as columns_list
+	from INFORMATION_SCHEMA.COLUMNS
+	group by
+		TABLE_CATALOG,
+		TABLE_SCHEMA,
+		TABLE_NAME
+)
+select
+	*,
+	concat('select ',
+			columns_list,
+			' from ',
+			TABLE_SCHEMA,
+			'.', 
+			TABLE_NAME,
+			';'
+		) as cmd_select,
+		concat('insert into ', TABLE_SCHEMA, '.', TABLE_NAME, ' (', columns_list, ')') as cmd_insert
+from list;
