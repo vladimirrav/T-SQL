82b33aa76de4a61ddb37d280aabb624129281ebd
vladimirrav <vladimir.rav@gmail.com>
2021-04-19 22:05:17 -0300

    Ajustes.

--- Cálculo da autonomia do veículo com gasolina e etanol.sql
+++ Cálculo da autonomia do veículo com gasolina e etanol.sql
@@ -7,13 +7,18 @@ declare @preco_g as decimal(19, 3),
 		@valor_km_e as decimal(19, 2),
 		@litros as decimal(19, 2),
 		@autonomia_g as decimal(19, 2) = 16.0,
-		@autonomia_e as decimal(19, 2) = 5.0;
+		@autonomia_e as decimal(19, 2) = 12.0;
 
 select	@preco_g = 5.738,
 		@valor = 112.86,
 		@litros = 50; --50; /* Se o valor for informado este é utilizado no cálculo ao invés de @valor */
 
-print concat('Consumo', char(10), char(9), 'Gasolina: ', @autonomia_g, ' Km/L', /*char(10),*/ char(9), 'Etanol: ', @autonomia_e, ' Km/L', char(10))
+print concat
+(
+	'Consumo', char(10), char(9),
+	'Gasolina: ', @autonomia_g, ' Km/L', /*char(10),*/ char(9),
+	'Etanol: ', @autonomia_e, ' Km/L', char(10)
+);
 
 set @valor = iif(nullif(@litros, 0) is not null, @litros * @preco_g, @valor);
 set @litros = isnull(nullif(@litros, 0), @valor / @preco_g);
@@ -22,9 +27,9 @@ set @autonomia_g = convert(decimal(19, 2), @autonomia_g * @litros);
 
 print concat
 (	'Cálulo da autonomia com gasolina', char(10), char(9),
-	'Preco/L: ', @preco_g, char(10), char(9),
-	'Preco/Km: ', @valor_km_g, char(10), char(9),
-	'Valor: ', @valor, char(10), char(9),
+	'Preço/L: R$ ', @preco_g, char(10), char(9),
+	'Preço/Km: R$ ', @valor_km_g, char(10), char(9),
+	'Valor: R$ ', @valor, char(10), char(9),
 	'Litros: ', @litros, char(10), char(9),
 	'Autonomia: ', @autonomia_g, ' Km', char(10)
 );
@@ -42,9 +47,9 @@ set @autonomia_e = convert(decimal(19, 2), @autonomia_e * @litros);
 
 print concat
 (	'Cálulo da autonomia com etanol', char(10), char(9),
-	'Preco/L: ', @preco_e, char(10), char(9),
-	'Preco/Km: ', @valor_km_e, char(10), char(9),
-	'Valor: ', @valor, char(10), char(9),
+	'Preço/L: R$ ', @preco_e, char(10), char(9),
+	'Preço/Km: R$ ', @valor_km_e, char(10), char(9),
+	'Valor: R$ ', @valor, char(10), char(9),
 	'Litros: ', @litros, char(10), char(9),
 	'Autonomia: ', @autonomia_e, ' Km', char(10)
 );
