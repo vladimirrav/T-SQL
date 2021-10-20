3d7d58d9c407b7390f5c79f43750d46a220cb553
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-10-20 12:41:00 -0300

    Update SQL Server Orphaned users.sql

--- SQL Server Orphaned users.sql
+++ SQL Server Orphaned users.sql
@@ -10,11 +10,16 @@ begin catch
 	print ''User: ', name, ''';
 	print concat(''error_number: '', error_number());
 	print concat(''error_message: '', error_message());
-end catch')
+end catch') as cmd,
+	concat('--create user ', name, ' for login ', name, ';
+alter role db_datareader add member ', name, ';') as usr,
 	name,
 	sid,
-	principal_id, *
+	principal_id,
+	*
 from sys.database_principals 
 where type = 's' 
   and name not in ('guest', 'information_schema', 'sys')
   and authentication_type_desc = 'instance';
+
+--execute sp_change_users_login update_one, 'usr', 'usr';
